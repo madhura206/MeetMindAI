9db6be7d70e644350450e6f41471c0e5e32f1b1d
madhura206 <madhurahawelikar7@gmail.com>
2026-07-28 21:34:56 +0530

    Add authentication controller

---
+++ backend/src/main/java/com/meetmind/backend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.meetmind.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.meetmind.backend.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.meetmind.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+
+        userService.registerUser(user);
+
+        return "User registered successfully";
+
+    }
+
+}
